178aae885972ca2f0651fe9c40644169587d32ec
Simeone-Holanda <simeone.10s10@gmail.com>
2022-03-20 04:17:47 -0300

    Criado o componente da tela de login.

--- front-end/src/components/pages/login/Login.jsx
+++ front-end/src/components/pages/login/Login.jsx
@@ -1,6 +1,28 @@
+import NavTop from "../../layout/NavTop/NavTop";
+import styles from './Login.module.css'
+import ScreenLogin from '../../../img/ScreenLogin.png'
+import FormLogin from "../../layout/form/screen/FormLogin";
 
 const Login = () => {
-    return (<h1>Login</h1>);
+
+    return (
+        <>
+            <div className={styles.container_login}>
+                <div className={styles.container_login}>
+                    <img src={ScreenLogin} alt="" />
+                    <div className={styles.form}>
+                        <div className={styles.container_infos}>
+                            <h1> Olá! Bem vindo de volta</h1>
+                            <p>Faça login com seus dados inseridos
+                                durante o seu registro.
+                            </p>
+                        </div>
+                        <FormLogin btnText="Login" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Login;
