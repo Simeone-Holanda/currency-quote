d8f9a792b3775dbce5eac7c8374443b5d16956bb
Simeone-Holanda <simeone.10s10@gmail.com>
2022-03-20 04:28:06 -0300

    Correções no layout e no NavTop do site.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,17 +3,21 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './components/pages/login/Login'
 import Register from './components/pages/register/Register'
 import Dashboard from './components/pages/dashboard/Dashboard'
+import NavTop from './components/layout/NavTop/NavTop'
 
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/dashboard' element={<Dashboard />} />
-    </Routes>
+    <>
+      <NavTop />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ front-end/src/components/layout/NavTop/NavTop.jsx
@@ -0,0 +1,22 @@
+import styles from './NavTop.module.css'
+import Logo_Beeteller from '../../../img/Logo_Beeteller.png'
+import Vector from '../../../img/Vector.png'
+import Container from '../container/Container';
+
+const NavTop = () => {
+    return (
+        <div className={styles.container_nav}>
+            <div className={styles.headers}>
+                <div className={styles.logo}>
+                    <img src={Logo_Beeteller} alt="" />
+                </div>
+                <span>COTAÇÕES</span>
+                <img src={Vector} alt="" />
+            </div>
+            <div className={styles.idioma}>EN</div>
+
+        </div >
+    );
+}
+
+export default NavTop;
