43d8903c19ccbfe4242b29fe4e10e4c923d81e25
Simeone-Holanda <simeone.10s10@gmail.com>
2022-03-20 04:21:00 -0300

    Criado a tela de cadastro de usuário.

--- front-end/src/components/pages/register/Register.jsx
+++ front-end/src/components/pages/register/Register.jsx
@@ -1,6 +1,26 @@
+import NavTop from "../../layout/NavTop/NavTop";
+import styles from '../login/Login.module.css';
+import ScreenLogin from '../../../img/ScreenLogin.png';
+import FormRegister from "../../layout/form/screen/FormRegister";
 
 const Register = () => {
-    return (<h1> Register</h1>);
+    return (
+        <>
+            <div>
+                <div className={styles.container_login}>
+                    <img src={ScreenLogin} alt="" />
+                    <div className={styles.form}>
+                        <div className={styles.container_infos}>
+                            <h1> Olá! Cadastra-se </h1>
+                            <p>Preencha os dados abaixo para concluir o cadastro.
+                            </p>
+                        </div>
+                        <FormRegister btnText="Cadastre-se" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Register;
