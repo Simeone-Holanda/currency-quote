cd9f960df485c1668de40d8f6a4747a580953f2d
Simeone-Holanda <simeone.10s10@gmail.com>
2022-03-22 10:44:02 -0300

    Conectando os CardCoin ao back-end e feito melhorias no código.

--- front-end/src/components/layout/CardCoin/CardCoin.jsx
+++ front-end/src/components/layout/CardCoin/CardCoin.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
 import styles from './CardCoin.module.css'
 
-const CardCoin = ({ coin1, coin2, handleValueCoin, CoinIcon }) => {
+const CardCoin = ({ coin1, coin2, CoinIcon, onChange }) => {
+
+    const [valueCoin, setValueCoin] = useState(0)
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:8000/convetion/${coin1}-${coin2}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(data => data.json())
+            .then(resp => {
+                console.log(resp)
+                setValueCoin(resp['result'])
+            })
+            .catch(e => console.log(e))
+    }, [onChange])
+
     return (
         <div className={styles.container_card_coin}>
             <div>
                 <p>{coin1} / {coin2}</p>
-                <h1> <span>R$</span> 5,30</h1>
+                <h1> <span>R$</span> {valueCoin}</h1>
             </div>
             <div>
                 <img src={CoinIcon} alt="" />
