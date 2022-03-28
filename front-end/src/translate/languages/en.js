8e79451235d508eb19a1a4e4f2b3ef376ef414c2
Simeone-Holanda <simeone.10s10@gmail.com>
2022-03-28 09:18:35 -0300

    Configurando a opção de traduzir textos do site.

---
+++ front-end/src/translate/languages/en.js
@@ -0,0 +1,41 @@
+const messages = {
+    en: {
+        translations: {
+            titles: {
+                login: 'Hi! Welcome back',
+                register: 'Hi! sign up'
+            },
+            subtitles: {
+                login: 'Log in with your data entered during your registration.',
+                register: 'Fill in the details below to complete the registration.'
+            },
+            navigationPageLink: {
+                login: 'Register',
+                register: 'I already have an account',
+                forgot_password: 'Forgot Password?'
+            },
+            buttons: {
+                login: 'Login',
+                register: 'Register'
+            },
+            inputs: {
+                name: 'Username:',
+                email: 'Email:',
+                password: 'Password:',
+                password_again: 'Password Again:'
+            },
+            dashboard: {
+                coins: 'Coins',
+                quotation: 'Quotations'
+            },
+            table: {
+                coin: 'Moeda',
+                min: 'Minimum',
+                max: 'Maximum',
+                variation: 'Variation',
+            }
+        }
+    }
+}
+
+export { messages }
---
+++ front-end/src/translate/languages/pt.js
@@ -0,0 +1,41 @@
+const messages = {
+    pt: {
+        translations: {
+            titles: {
+                login: 'Olá! Bem vindo de volta',
+                register: 'Olá! Cadastra-se.'
+            },
+            subtitles: {
+                login: 'Faça login com seus dados inseridos durante o seu registro.',
+                register: 'Preencha os dados abaixo para concluir o cadastro..'
+            },
+            navigationPageLink: {
+                login: 'Cadastre-se',
+                register: 'Já possuo uma conta',
+                forgot_password: 'Esqueceu a senha? '
+            },
+            buttons: {
+                login: 'Conecte-se',
+                register: 'Cadastre-se'
+            },
+            inputs: {
+                name: 'Nome de usuário:',
+                email: 'Email:',
+                password: 'Senha:',
+                password_again: 'Senha novamente:'
+            },
+            dashboard: {
+                coins: 'Moedas',
+                quotation: 'Cotações'
+            },
+            table: {
+                coin: 'Moeda',
+                min: 'Minimo',
+                max: 'Maximo',
+                variation: 'Variação',
+            }
+        }
+    }
+}
+
+export { messages }
