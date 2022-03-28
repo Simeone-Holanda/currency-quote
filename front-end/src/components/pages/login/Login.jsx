4844b69b12b02db71efb0bbad4cf5a118e9108cd
Simeone-Holanda <simeone.10s10@gmail.com>
2022-03-27 22:03:51 -0300

    Conectando a api para fazer login/ Adicionando componente de mensagem.

--- front-end/src/components/pages/login/Login.jsx
+++ front-end/src/components/pages/login/Login.jsx
@@ -1,12 +1,45 @@
-import NavTop from "../../layout/NavTop/NavTop";
 import styles from './Login.module.css'
+import { useLocation } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+import Message from "../../layout/Message/Message";
 import ScreenLogin from '../../../img/ScreenLogin.png'
 import FormLogin from "../../layout/form/screen/FormLogin";
+import { useState } from 'react';
+
 
 const Login = () => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const [message, setMessage] = useState('')
+    const [type, setType] = useState('')
+
+    if (location.state) {
+        setMessage(location.state.message)
+    }
+
+    function loginUser(dataLogin) {
+        setMessage('')
+        fetch('http://127.0.0.1:8000/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataLogin)
+        }).then(data => data.json())
+            .then(resp => {
+                if (resp.status == '202') {
+                    navigate('/dashboard', { state: resp })
+                } else {
+                    setMessage(resp.message)
+                    setType('error')
+                }
+
+            })
+    }
 
     return (
         <>
+            {message && <Message type={type} msg={message} />}
             <div className={styles.container_login}>
                 <div className={styles.container_login}>
                     <img src={ScreenLogin} alt="" />
@@ -17,7 +50,7 @@ const Login = () => {
                                 durante o seu registro.
                             </p>
                         </div>
-                        <FormLogin btnText="Login" />
+                        <FormLogin handleSubmit={loginUser} btnText="Login" />
                     </div>
                 </div>
             </div>
